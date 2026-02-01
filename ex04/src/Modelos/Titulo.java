1a75cb3eb770c9aa34bd68e23261a9244986d19a
Ruan Almeida <ruan.aalmeidasantos@gmail.com>
2026-02-01 19:41:20 -0300

    Collections.sort

---
+++ ex04/src/Modelos/Titulo.java
@@ -0,0 +1,14 @@
+package Modelos;
+
+public class Titulo implements Comparable<Titulo> {
+    public String nome;
+
+    public Titulo(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.nome.compareTo(outroTitulo.nome);
+    }
+}
